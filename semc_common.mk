f5b563c775a40e04da524b8fe6351cc96ca95dc8
MarcinDevelopment <nulln785@gmail.com>
2018-07-02 17:29:42 +0200

    Initial commit

---
+++ semc_common.mk
@@ -0,0 +1,31 @@
+# Copyright 2018 outofmemory13@XDA | Xperia for AOSP 
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Xperia (XZ) Bootanimation
+PRODUCT_COPY_FILES += \
+    vendor/semc/prebuilt/semc/media/bootanimation.zip:system/media/bootanimation.zip
+
+# Xperia Keyboard
+PRODUCT_COPY_FILES += \
+    vendor/semc/prebuilt/semc/app/XperiaKeyboard/XperiaKeyboard.apk:system/app/XperiaKeyboard/XperiaKeyboard.apk \
+    vendor/semc/prebuilt/semc/app/XperiaKeyboard/lib/arm/libswiftkeysdk-java.so:system/app/XperiaKeyboard/lib/arm/libswiftkeysdk-java.so
+
+# Xperia Home
+PRODUCT_PACKAGES += \
+    home-release \
+    SemcExactCalculator \
+    semc-organizer
+
+# Xperia Overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
